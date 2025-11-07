aa440c9c133ca6ed6447596f42f7365c463a4f6b
sharmilanaik1969-source <sharmilanaik1969@gmail.com>
2025-11-07 09:16:54 +0530

    Update WelcomeScreen.tsx

--- WelcomeScreen.tsx
+++ WelcomeScreen.tsx
@@ -1,1 +1,19 @@
-Assesments/src/components/WelcomeScreen.tsx
+import React from "react";
+
+export default function WelcomeScreen() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Sharmila’s Divine Blessings</h1>
+      <p>Welcome to the Chakra Meditation App ✨</p>
+      <ul>
+        <li>Root Chakra</li>
+        <li>Sacral Chakra</li>
+        <li>Solar Plexus</li>
+        <li>Heart Chakra</li>
+        <li>Throat Chakra</li>
+        <li>Third Eye</li>
+        <li>Crown Chakra</li>
+      </ul>
+    </div>
+  );
+}
